26883c29aaca6444a4b183bf3d90c24e2df08bc5
stangjr19 <650112418020@bru.ac.th>
2023-11-23 16:35:11 +0700

    LAB3

---
+++ LAB3/html/condb.php
@@ -0,0 +1,17 @@
+<?php
+$host = "db";
+$username = "root";
+$password = "1234";
+$db = "internet";
+
+try{
+    $conn = mysqli_connect($host,$username,$password,$db);
+    mysqli_query($conn,"SET NAME utf8");
+}
+catch(Exception $e){
+    $error = $e->getMessage();
+    error_log($error);
+    echo $error;
+}
+
+?>
